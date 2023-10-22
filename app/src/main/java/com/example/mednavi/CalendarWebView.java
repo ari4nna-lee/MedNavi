4dd88cf13fe3674af2cda6707afc1ec87c64a001
Arianna <ajlee338@gmail.com>
2023-10-22 01:31:26 -0400

    added main menu panel and customized some colors

--- app/src/main/java/com/example/mednavi/CalendarWebView.java
+++ app/src/main/java/com/example/mednavi/CalendarWebView.java
@@ -3,12 +3,20 @@ package com.example.mednavi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class CalendarWebView extends AppCompatActivity {
+
+    WebView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_web_view);
+        calendar = findViewById(R.id.calendar_webview);
+        WebSettings webSettings = calendar.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        calendar.loadUrl("https://calendar.google.com/calendar/embed?src=mednavihelp%40gmail.com&ctz=America%2FNew_York");
     }
 }
--- app/src/main/java/com/example/mednavi/MainActivity.java
+++ app/src/main/java/com/example/mednavi/MainActivity.java
@@ -1,31 +1,35 @@
 package com.example.mednavi;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button logoutButton;
-    Button goToMaps;
     FirebaseFirestore fStore;
     String userID;
     FirebaseAuth auth;
     FirebaseUser user;
+    TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logoutButton = findViewById(R.id.logout);
-        goToMaps = findViewById(R.id.goToMaps);
+        welcomeText = findViewById(R.id.main_title);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
@@ -33,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
 
         userID = auth.getCurrentUser().getUid();
 
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        ListenerRegistration registration = documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                welcomeText.setText("welcome back, " + documentSnapshot.getString("fName") + "!");
+            }
+        });
+
+        /*
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,11 +59,21 @@ public class MainActivity extends AppCompatActivity {
         goToMaps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                auth.signOut();
                 Intent intent = new Intent(getApplicationContext(), GoogleResourceMap.class);
                 startActivity(intent);
                 finish();
             }
         });
+
+        goToCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CalendarWebView.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+         */
     }
 }
